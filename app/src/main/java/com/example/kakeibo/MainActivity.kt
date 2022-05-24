2d68bf18db6fbeb295f3eaf2db9a4deba1254cc4
Futoren <kajimotoda1004@outlook.jp>
2022-05-24 19:50:45 +0900

    camera finish2

--- app/src/main/java/com/example/kakeibo/MainActivity.kt
+++ app/src/main/java/com/example/kakeibo/MainActivity.kt
@@ -6,15 +6,25 @@ import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.ContextCompat
 import com.example.kakeibo.databinding.ActivityMainBinding
-import android.content.Intent;
+import android.content.Intent
 import android.graphics.Bitmap
-import android.provider.MediaStore;
+import android.graphics.BitmapFactory
+import android.net.Uri
+import android.os.Environment
+import android.provider.MediaStore
 import android.util.Log
 import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.content.FileProvider
+import java.io.File
+import java.io.IOException
+import java.text.SimpleDateFormat
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
-    val REQUEST_CAMERA_PERMISSION = 1
+    private val REQUEST_CAMERA_PERMISSION = 1
+    private val REQUEST_IMAGE_CAPTURE = 100
+    private lateinit var currentPhotoPath: String
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -36,20 +46,46 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun dispatchTakePictureIntent() {
-        Log.d("check4","dispatch")
         Intent(MediaStore.ACTION_IMAGE_CAPTURE).also { takePictureIntent ->
-            Log.d("check5","dispatch")
-            picture.launch(takePictureIntent)
+                val photoFile: File? = try {
+                    createImageFile()
+                } catch (ex: IOException) {
+                    null
+                }
+                photoFile?.also {
+                    val photoURI: Uri = FileProvider.getUriForFile(
+                        this,
+                        "com.example.android.fileprovider",
+                        it
+                    )
+//                  どちらしか行えない値を使うと空になってしまう
+                    takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI)
+                    showPicture.launch(takePictureIntent)
+                }
         }
     }
 
-    private var picture = registerForActivityResult(ActivityResultContracts.StartActivityForResult())
-    { result ->
-        Log.d("check3","picture")
+    private var showPicture = registerForActivityResult(
+        ActivityResultContracts.StartActivityForResult()) { result ->
         if (result.resultCode == RESULT_OK) {
             val data: Intent? = result.data
             val imageBitmap = data?.extras?.get("data") as Bitmap
             binding.imageView.setImageBitmap(imageBitmap)
+        }
+    }
+
+    @Throws(IOException::class)
+    private fun createImageFile(): File {
+        // Create an image file name
+        val timeStamp: String = SimpleDateFormat("yyyyMMdd_HHmmss").format(Date())
+        val storageDir: File? = getExternalFilesDir(Environment.DIRECTORY_PICTURES)
+        return File.createTempFile(
+            "JPEG_${timeStamp}_", /* prefix */
+            ".jpg", /* suffix */
+            storageDir /* directory */
+        ).apply {
+            // Save a file: path for use with ACTION_VIEW intents
+            currentPhotoPath = absolutePath
         }
     }
 }
