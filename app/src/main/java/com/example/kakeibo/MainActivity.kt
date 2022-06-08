8ccf9be931921dd38d11ca43f658c18de8b58514
Futoren <kajimotoda1004@outlook.jp>
2022-06-08 16:06:10 +0900

    finish

--- app/src/main/java/com/example/kakeibo/MainActivity.kt
+++ app/src/main/java/com/example/kakeibo/MainActivity.kt
@@ -5,6 +5,7 @@ import android.content.Intent
 import android.content.pm.PackageManager
 import android.graphics.Bitmap
 import android.graphics.BitmapFactory
+import android.graphics.Color
 import android.graphics.Matrix
 import android.icu.text.SimpleDateFormat
 import android.net.Uri
@@ -28,7 +29,7 @@ class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
     private val REQUEST_CAMERA_PERMISSION = 1
     private lateinit var currentPhotoPath: String
-    private var sumNumber : Int = 0
+    private var sumNumber : Long = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -43,7 +44,6 @@ class MainActivity : AppCompatActivity() {
         binding.buttonCapture.setOnClickListener { buttonCapture() }
         binding.addButton.setOnClickListener{addValue()}
         binding.setLimit.setOnClickListener { setLimit() }
-        imageReaderNew()
     }
 
     private fun buttonCapture(){
@@ -52,6 +52,13 @@ class MainActivity : AppCompatActivity() {
 
     private fun addValue(){
         binding.sumValue.text=sumNumber.toString()
+        if(sumNumber>binding.limitValue.text.toString().toLong()){
+            binding.checkValue.text="Over"
+            binding.checkValue.setTextColor(Color.RED)
+        }else{
+            binding.checkValue.text="Save"
+            binding.checkValue.setTextColor(Color.GREEN)
+        }
     }
 
     private fun setLimit(){
@@ -74,7 +81,16 @@ class MainActivity : AppCompatActivity() {
     }
 
     private var showPicture = registerForActivityResult(
-        ActivityResultContracts.StartActivityForResult()){}
+        ActivityResultContracts.StartActivityForResult()){
+        if(it.resultCode==0){
+            val filepath = File("/storage/self/primary/" +
+                    "Android/data/com.example.kakeibo/files/Pictures")
+            val imageFile = filepath.walk().filter { it.name.endsWith(".jpeg") }.last()
+            imageFile.delete()
+        }else{
+            imageReaderNew()
+        }
+    }
 
     @Throws(IOException::class)
     private fun createImageFile(): File? {
@@ -84,11 +100,11 @@ class MainActivity : AppCompatActivity() {
         if (storageDir != null) {
             Log.d("storageDirSize",storageDir.length().toString())
         }
-        if (storageDir != null&&storageDir.length()>0){
-            return File.createTempFile("JPEG_${timeStamp}_",".jpeg", storageDir
+        return if (storageDir != null&&storageDir.length()>0){
+            File.createTempFile("JPEG_${timeStamp}_",".jpeg", storageDir
             ).apply { currentPhotoPath = absolutePath }
         }else{
-            return null
+            null
         }
     }
 
@@ -109,13 +125,17 @@ class MainActivity : AppCompatActivity() {
         val recognizer = TextRecognizer.Builder(this).build()
         val frame = Frame.Builder().setBitmap(image).build()
         val textBlockSparseArray = recognizer.detect(frame)
-        var maxTax = 0
-        var tax:Int
+        var maxTax:Long = 0
+        var tax:Long
         for (i in 0 until textBlockSparseArray.size()) {
             val textBlock = textBlockSparseArray.valueAt(i)
             if(Regex("¥").containsMatchIn(textBlock.value.toString())) {
-                tax = textBlock.value.toString().replace("[^0-9]".toRegex(), "")
-                    .toInt()
+                try {
+                    tax = textBlock.value.toString().replace("[^0-9]".toRegex(), "")
+                        .toLong()
+                }catch(e:NumberFormatException){
+                    tax = 0
+                }
                 if(tax>maxTax) {maxTax=tax}
             }
         }
